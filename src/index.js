b822182111417828a4909daf80a2e49850300285
tongchuan <tongchuanxing@163.com>
2020-12-04 09:23:58 +0800

    ddd

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import '@babel/polyfill'
 import Koa from 'koa'
 
 const app = new Koa()
-app.use(ctx => {
+app.use((ctx) => {
   ctx.body = 'Hello Koa111'
 })
-		console.log('22222', 333)
-console.log('222')
+console.log('22222', 333)
+	console.log('222')
 
 app.listen(3000)
